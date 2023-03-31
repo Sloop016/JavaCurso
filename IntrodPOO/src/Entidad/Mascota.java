d7bcb98d87644718180b0748906e0dfa56500c71
Sobrino Gabriel <gabyezeiza22@gmail.com>
2023-03-31 08:51:25 -0300

    intro

---
+++ IntrodPOO/src/Entidad/Mascota.java
@@ -0,0 +1,105 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Entidad;
+
+/**
+ *
+ * @author gabyg
+ */
+public class Mascota {
+
+    //------------------ atributos -----------------------------------------------------------------
+    private String nombre;
+    private String apodo;
+    private String tipo;
+    private String color;
+    private int edad;
+    private boolean cola;
+    private String raza;
+
+    //------------------ constructor ----------------------------------------------------------------
+    public Mascota() {
+    }
+
+    public Mascota(String nombre, String apodo, String tipo) {
+        this.nombre = nombre;
+        this.apodo = apodo;
+        this.tipo = tipo;
+
+    }
+
+    public Mascota(String nombre, String apodo, String tipo, String color, int edad, boolean cola, String raza) {
+        this.nombre = nombre;
+        this.apodo = apodo;
+        this.tipo = tipo;
+        this.color = color;
+        this.edad = edad;
+        this.cola = cola;
+        this.raza = raza;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApodo(String apodo) {
+        this.apodo = apodo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public void setCola(boolean cola) {
+        this.cola = cola;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApodo() {
+        return apodo;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public boolean isCola() {
+        return cola;
+    }
+
+    public String getRaza() {
+        return raza;
+    }
+
+    //---------------------------------------------------------------------------------------------
+    @Override
+    public String toString() {
+        return "Mascota{" + "nombre=" + nombre + ", apodo=" + apodo + ", tipo=" + tipo + ", color=" + color + ", edad=" + edad + ", cola=" + cola + ", raza=" + raza + '}';
+    }
+
+}
---
+++ IntrodPOO/src/person/MascotaApp.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package person;
+
+import Entidad.Mascota;
+
+/**
+ *
+ * @author gabyg
+ */
+public class MascotaApp {
+
+    public static void main(String[] args) {
+        
+        Mascota m1 = new Mascota();
+        
+        
+        
+    }
+}
